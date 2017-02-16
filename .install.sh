f2d63fc841435bba0e1a9dc93d56bbe1859cfaee
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-16 17:23:43 +0800

    Updated webpack configuration.

---
+++ .install.sh
@@ -0,0 +1,23 @@
+yarn add \
+babel-runtime \
+kurse \
+plough \
+sxty4 \
+truly \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+kurse \
+plough \
+sxty4 \
+truly \
+zelf \
+--save
---
+++ pringe-test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const pringe = require( "./pringe.js" );
+
+console.log( pringe( ) );
