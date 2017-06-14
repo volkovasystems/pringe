c9e7fead33692a167b80564046bd7d3edcded77d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-14 14:51:43 +0800

    Save changes.

--- pringe-test.js
+++ pringe-test.js
@@ -1,6 +1,8 @@
 const assert = require( "assert" );
 const pringe = require( "./pringe.js" );
 
-assert.ok( pringe( ) );
+assert.equal( pringe( "hello" ), "dW5kZWZpbmVkQChoZWxsbyk%3D", "should be equal" );
+
+assert.equal( pringe( { "name": "simple" } ), "dW5kZWZpbmVkQChbb2JqZWN0IE9iamVjdF0p", "should be equal" );
 
 console.log( "ok" );
