1806b9ee92b968a507e91c1aeb88c83c7749ad46
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-16 20:52:03 +0800

    Update client/server unit test.

--- pringe-test.js
+++
@@ -1,9 +0,0 @@
-
-const assert = require( "assert" );
-const pringe = require( "./pringe.js" );
-
-assert.equal( typeof pringe( "hello" ), "string", "should be equal to 'string'" );
-
-assert.equal( typeof pringe( { "name": "simple" } ), "string", "should be equal to 'string'" );
-
-console.log( "ok" );
